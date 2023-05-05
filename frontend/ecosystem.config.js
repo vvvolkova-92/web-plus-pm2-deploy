fe7b578f28d10d8ba9faae3d93814ddfcbcbc853
vvvolkova-92 <saiviolet@gmail.com>
2023-05-05 17:50:36 +0300

    .env.deploy для фронта и бэка

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,25 @@
+require('dotenv').config({
+  path: '.env.deploy',
+});
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REP, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend',
+    script: './build/index.html',
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REP,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
