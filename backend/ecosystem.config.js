cce24e0f5ebade34b24b8fd25958a896c0081064
vvvolkova-92 <saiviolet@gmail.com>
2023-05-09 17:20:19 +0300

    3

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,6 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REP,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'cd /source/backend && npm i && npm run build &&  pm2 startOrRestart ecosystem.config.js',
     },
   },
