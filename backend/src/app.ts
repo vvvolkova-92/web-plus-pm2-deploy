d6b3b2cb971b5b304d73d20a3d95a403e633ccc4
vvvolkova-92 <saiviolet@gmail.com>
2023-05-13 18:46:42 +0300

    readme, cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -13,8 +13,12 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
-// Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+app.use(cors({
+  origin: [
+    'https://vilo4ka.ru',
+    'http://vilo4ka.ru',
+  ],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
